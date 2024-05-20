60f625c35dd0fbe60caec849cb5b14557442228d
parker0509 <167055018+parker0509@users.noreply.github.com>
2024-05-20 18:14:43 +0900

    꽃 홈페이지

---
+++ src/main/java/com/Flower/flowerShop/Controller/UserController.java
@@ -0,0 +1,54 @@
+package com.Flower.flowerShop.Controller;
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("name","parker");
+        return "index";
+    }
+  /*  @GetMapping("/product")
+    public String product(){
+        return "product";
+    }*/
+    @GetMapping("/productCheck")
+    public String product(){
+        return "productCheck";
+    }
+
+    @GetMapping("/upload_Product_form")
+    public String uploadProduct(){
+        return "/upload_Product_form";
+    }
+
+    @GetMapping("/read_Product_form")
+    public String readProduct(){
+        return "/read_Product_form";
+    }
+
+    @GetMapping("readProductForm")
+    public String readProductCheck(){
+        return "/readProductForm";
+    }
+
+    @GetMapping("/join")
+    public String joinUser(){return "/join"; }
+/*
+
+    @PostMapping("/join")
+    public String joinHome(){return"join";}
+*/
+
+}
---
+++ src/main/java/com/Flower/flowerShop/DTO/Product.java
@@ -0,0 +1,24 @@
+package com.Flower.flowerShop.DTO;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productName;
+    private int productPrice;
+
+}
