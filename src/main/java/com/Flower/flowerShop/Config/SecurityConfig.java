4fbc8aaf9dc96a41a4e88b8bfbeb59c23bf65096
parker0509 <167055018+parker0509@users.noreply.github.com>
2024-05-28 15:45:42 +0900

    :anguished: SecurityConfig

---
+++ src/main/java/com/Flower/flowerShop/Config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.Flower.flowerShop.Config;
+
+import com.Flower.flowerShop.Service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+             .authorizeRequests((authz) -> authz
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/index").authenticated()
+                .anyRequest().authenticated()
+             );
+        return http.build();
+    }
+
+}
+
