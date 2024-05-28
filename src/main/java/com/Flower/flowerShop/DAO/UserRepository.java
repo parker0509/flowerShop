4d1ec26e1f384390c6f1314248adf50046253560
parker0509 <167055018+parker0509@users.noreply.github.com>
2024-05-28 15:40:57 +0900

    :sunny: 이메일찾기 기능 추가

--- src/main/java/com/Flower/flowerShop/DAO/UserRepository.java
+++ src/main/java/com/Flower/flowerShop/DAO/UserRepository.java
@@ -5,8 +5,11 @@ import com.Flower.flowerShop.DTO.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
+    Optional<User> findByMemberEmail(String username);
 }
