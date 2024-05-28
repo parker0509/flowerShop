c712e949338c9db573596bbb46f63603f9334c77
parker0509 <167055018+parker0509@users.noreply.github.com>
2024-05-28 15:38:50 +0900

    :sunny: User도메인 변경

--- src/main/java/com/Flower/flowerShop/DTO/User.java
+++ src/main/java/com/Flower/flowerShop/DTO/User.java
@@ -1,6 +1,8 @@
 package com.Flower.flowerShop.DTO;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,10 +17,15 @@ public class User {
 
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //ID
     private Long id;
-
-    private String name;
-    private int Number;
-    private int goodsNumber;
-
+    //NICKNAME
+    private String nickname;
+    //EMAIL
+    private String memberEmail;
+    //PASSWORD
+    private String password;
+    //AccountType:타입유형 (example=구글)
+    private String accountType;
 }
